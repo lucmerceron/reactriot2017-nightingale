374a529f9954bbf22b948896a21fd7aec0c5361f
Yohann Craumacher <yomansk8@gmail.com>
2017-06-24 15:24:08 +0200

    Add FeedList component

---
+++ src/components/feedPanel/FeedList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './FeedList.css'
+
+const FeedList = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+FeedList.propTypes = {
+
+}
+
+export default FeedList
