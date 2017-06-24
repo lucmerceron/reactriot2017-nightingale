1f323a731acb9c8f6d897a197be9ddb7f641292c
Yohann Craumacher <yomansk8@gmail.com>
2017-06-24 15:57:34 +0200

    Updated FeedList Component

--- src/components/feedPanel/FeedList.js
+++ src/components/feedPanel/FeedList.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import FeedListItem from './FeedListItem'
+import FeedListAddMusicContent from './FeedListAddMusicContent'
+
 import './FeedList.css'
 
 const FeedList = props => {
-  return (
-    <div>
+  const { musicsFeed } = props
 
-    </div>
+  return (
+    <ul>
+      {
+        musicsFeed.map(musicFeed => <FeedListItem><FeedListAddMusicContent musicFeed={musicFeed} /></FeedListItem>)
+      }
+    </ul>
   )
 }
 
 FeedList.propTypes = {
-
+  musicsFeed: PropTypes.array(PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })).isRequired,
 }
 
 export default FeedList
