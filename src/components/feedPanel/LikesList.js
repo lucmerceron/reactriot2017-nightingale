3ee8e81a8de36a0e7f9aa79fe3b3a22d07793b8e
Yohann Craumacher <yomansk8@gmail.com>
2017-06-24 16:00:32 +0200

    Updated LikesList Component

--- src/components/feedPanel/LikesList.js
+++ src/components/feedPanel/LikesList.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import FeedListItem from './FeedListItem'
+import FeedListLikeMusicContent from './FeedListLikeMusicContent'
+
 import './LikesList.css'
 
 const LikesList = props => {
-  return (
-    <div>
+  const { likesFeed } = props
 
-    </div>
+  return (
+    <ul>
+      {
+        likesFeed.map(likeFeed => <FeedListItem><FeedListLikeMusicContent likeFeed={likeFeed} /></FeedListItem>)
+      }
+    </ul>
   )
 }
 
 LikesList.propTypes = {
-
+  likesFeed: PropTypes.array(PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })).isRequired,
 }
 
 export default LikesList
