c770e987209afa915959e418919788c7afc4b7b2
Yohann Craumacher <yomansk8@gmail.com>
2017-06-24 15:27:54 +0200

    Add LikesList component

---
+++ src/components/feedPanel/LikesList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './LikesList.css'
+
+const LikesList = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+LikesList.propTypes = {
+
+}
+
+export default LikesList
