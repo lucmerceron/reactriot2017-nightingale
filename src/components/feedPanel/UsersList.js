043a6ea984ccd000b9232c52aa89e5d5e770b31c
Yohann Craumacher <yomansk8@gmail.com>
2017-06-24 15:30:49 +0200

    Add UsersList component

---
+++ src/components/feedPanel/UsersList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './UsersList.css'
+
+const UsersList = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+UsersList.propTypes = {
+
+}
+
+export default UsersList
