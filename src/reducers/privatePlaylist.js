479a7ac6370242f07bfd15251845a1e537ef7651
Luc Merceron <luc.merceron@wing.eu>
2017-06-24 12:25:26 +0200

    [*] Private & Public playlists reducers

--- src/reducers/privatePlaylist.js
+++ src/reducers/privatePlaylist.js
@@ -1,6 +1,18 @@
-// Reducer for the privatePlaylist
-export default function privatePlaylist(state = {}, action) {
+import { extend } from 'lodash'
+
+import {
+  GET_PRIVATE_PLAYLIST_SUCCESS,
+  CREATE_PRIVATE_PLAYLIST_SUCCESS,
+} from '../actionCreators/playlists'
+
+export default function privatePlaylists(state = {}, action) {
   switch (action.type) {
+    case GET_PRIVATE_PLAYLIST_SUCCESS: {
+      return extend({}, state, action.playlist)
+    }
+    case CREATE_PRIVATE_PLAYLIST_SUCCESS: {
+      return extend({}, state, action.playlist)
+    }
     default:
       return state
   }
--- src/reducers/publicPlaylists.js
+++ src/reducers/publicPlaylists.js
@@ -1,6 +1,14 @@
-// Reducer for the publicPlaylists
+import { extend } from 'lodash'
+
+import {
+  UPDATE_PUBLIC_PLAYLIST,
+} from '../actionCreators/playlists'
+
 export default function publicPlaylists(state = {}, action) {
   switch (action.type) {
+    case UPDATE_PUBLIC_PLAYLIST: {
+      return extend({}, action.playlists)
+    }
     default:
       return state
   }
