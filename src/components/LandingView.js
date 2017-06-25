77eda7b8a3c5ab853318c1c810faa305a08214f8
Yann Jacquet <yannick.jacquet@gmail.com>
2017-06-25 22:20:38 +0200

    [*] Front page

--- src/components/LandingView.js
+++ src/components/LandingView.js
@@ -51,15 +51,14 @@ class LandingView extends Component {
 
     return (
       <div className="landing-view">
-        <video autoPlay loop poster="polina.jpg" className="landing-view-bg-video">
-          <source src={bgVideoUrl} type="video/mp4" />
-        </video>
         <div className="landing-view-actions-wrapper">
-          <div className={`landing-view-actions-background ${hasMorphed !== 0 ? 'morphed' : ''}`} />
           <div className={`landing-view-actions-background-position ${hasMorphed !== 0 ? 'button-morphed' : ''}`}>
-            <img />
-            {hasMorphed !== 2 && <ButtonMorphing label="join a playlist" onClick={() => this.setState({ hasMorphed: 1 })} content={<JoinPlaylistContent />} />}
-            {hasMorphed !== 1 && <ButtonMorphing label="create a playlist" onClick={() => this.setState({ hasMorphed: 2 })} content={<CreatePlaylistContent />} />}
+            {hasMorphed === 0 && <span className="ng-logo">Nightingale</span>}
+            {hasMorphed === 0 && <p className="ng-logo-subtitle">add . vote . listen</p>}
+            <div className="landing-view-actions-group">
+              {hasMorphed !== 2 && <ButtonMorphing label="join a playlist" onClick={() => this.setState({ hasMorphed: 1 })} content={<JoinPlaylistContent />} />}
+              {hasMorphed !== 1 && <ButtonMorphing label="create a playlist" onClick={() => this.setState({ hasMorphed: 2 })} content={<CreatePlaylistContent />} />}
+            </div>
           </div>
         </div>
         {hasMorphed === 0 && <span>developed by the Wing dev team with love</span>}
