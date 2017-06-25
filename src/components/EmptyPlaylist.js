dbdfd70b3f1ef13de9d0a5c1ebf1e57ba39d93bd
Yohann Craumacher <yomansk8@gmail.com>
2017-06-25 19:38:54 +0200

    Add playlist title to EmptyPlaylist component

--- src/components/EmptyPlaylist.js
+++ src/components/EmptyPlaylist.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './EmptyPlaylist.css'
 
-const EmptyPlaylist = () => (
+const EmptyPlaylist = ({ playlistName }) => (
   <div className="empty-playlist">
-    <div className="empty-playlist-title">This playlist is empty.</div>
+    <div className="empty-playlist-title">{playlistName}</div>
     <div className="empty-playlist-icon-big">
       <i className="ion-sad-outline" />
     </div>
     <div className="empty-playlist-subtitle">
-      Bring a little of life here.<br /><br />
-    Add some songs to the playlist with the help of the search box!<br />
+      This playlist is empty.<br /><br />
+      Add some songs to the playlist with the help of the search box!<br />
     </div>
   </div>
 )
+
+EmptyPlaylist.propTypes = {
+  playlistName: PropTypes.string.isRequired,
+}
 
 export default EmptyPlaylist
